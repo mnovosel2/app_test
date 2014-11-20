b9321ce6d35c716fe29da1dba00a825bd0f64c11
Mario Novoselec <m.novoselec1@gmail.com>
2014-11-21 00:49:11 +0100

    Default template finished

--- app/views/default/layout.blade.php
+++ app/views/default/layout.blade.php
@@ -7,24 +7,22 @@
     <!--<![endif]-->
     <head>
         <meta charset="UTF-8">
-        <title>Oleose App Landing Page | Bootstrap Theme</title>
+        <title>RSC App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <link rel="shortcut icon" href="favicon.png">
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <link rel="stylesheet" href="css/animate.css">
-        <link rel="stylesheet" href="css/font-awesome.min.css">
-        <link rel="stylesheet" href="css/slick.css">
-        <link rel="stylesheet" href="js/rs-plugin/css/settings.css">
-        <script type="text/javascript" src="js/modernizr.custom.32033.js"></script>
-        <link rel="stylesheet" href="css/eco.css">
+        <link rel="stylesheet" href="/assets/css/bootstrap.css">
+        <link rel="stylesheet" href="/assets/css/animate.css">
+        <link rel="stylesheet" href="/assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="/assets/css/slick.css">
+        <link rel="stylesheet" href="/assets/js/rs-plugin/css/settings.css">
+        <script type="text/javascript" src="/assets/js/modernizr.custom.32033.js"></script>
+        <link rel="stylesheet" href="/assets/css/eco.css">
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
     </head>
     <body>
-        <div class="container">
-            @yield('content')
-        </div>
+         @yield('content')
     </body>
